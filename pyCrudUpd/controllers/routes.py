b95cafcee0c1d752f8456f1c8326c453d2af433f
iruamfx <iruamfx@gmail.com>
2025-05-23 11:47:44 -0300

    @iruamfx

---
+++ pyCrudUpd/controllers/routes.py
@@ -0,0 +1,60 @@
+from flask import render_template, request, redirect, url_for
+from models.database import Game
+
+# Lista de jogadores
+jogadores = ['Miguel José', 'Miguel Isack', 'Leaf',
+             'Quemario', 'Trop', 'Aspax', 'maxxdiego']
+
+# Array de objetos - Lista de games
+gamelist = [{'Título': 'CS-GO',
+            'Ano': 2012,
+             'Categoria': 'FPS Online'}]
+
+
+def init_app(app):
+    # Criando a primeira rota do site
+    @app.route('/')
+    # Criando função no Python
+    def home():
+        return render_template('index.html')
+
+    # Rota de games
+    @app.route('/games', methods=['GET', 'POST'])
+    def games():
+        game = gamelist[0]
+        # Tratando se a requisição for do tipo POST
+        if request.method == 'POST':
+            # Verificar se o campo 'jogador' existe
+            if request.form.get('jogador'):
+                # O append adiciona o item a lista
+                jogadores.append(request.form.get('jogador'))
+            return redirect(url_for('games'))
+
+        jogos = ['Jogo 1', 'Jogo 2', 'Jogo 3', 'Jogo 4', 'Jogo 5', 'Jogo 6']
+        return render_template('games.html',
+                               game=game,
+                               jogadores=jogadores,
+                               jogos=jogos)
+    
+    # Rota de cadastro de jogos (em dicionário)
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gamelist.append({'Título' : request.form.get('titulo'), 'Ano' : request.form.get('ano'), 'Categoria' : request.form.get('categoria')})
+            return redirect(url_for('cadgames'))
+        return render_template('cadgames.html',
+                               gamelist=gamelist)
+        
+    # ROTA DO CRUD (Estoque de jogos)
+    @app.route('/estoque', methods=["GET", "POST"])
+    def estoque():
+        if request.method == "POST":
+            newgame = Game(request.form["titulo"], request.form["ano"], request.form["categoria"], request.form["plataforma", request.form["preco"]])
+            db.session.add(newgame)
+            db.session.commit()
+            return redirect(url_for("estoque"))
+        # ORM que estamos usando é a SQLAlchemy
+        # O método query.all = SELECT * from...
+        gamesEmEstoque = Game.query.all()        
+        return render_template('estoque.html', gamesEmEstoque=gamesEmEstoque)
