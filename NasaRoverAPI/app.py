a4eed54b0fae76432e4a4d2153dd1097c9b25b45
iruamfx <iruamfx@gmail.com>
2025-08-22 14:08:08 -0300

    @iruamfx

---
+++ NasaRoverAPI/app.py
@@ -0,0 +1,15 @@
+# Importando o Flask
+from flask import Flask, render_template
+# Importando o PyMySQL
+import pymysql
+# Importando as rotas que estão nos controllers
+from controllers import routes
+
+# Carregando o Flask na variável app
+app = Flask(__name__, template_folder='views')
+
+# Chamando as rotas
+routes.init_app(app)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
